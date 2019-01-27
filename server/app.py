e5d16e171b1c7c95a45d31a12a8f3ec7055aa659
Dan Young <DanYoung@Christinas-iMac.fios-router.home>
2019-01-26 20:08:30 -0500

    updates

--- server/app.py
+++ server/app.py
@@ -8,14 +8,14 @@ redis_host = "localhost"
 redis_port = 6379
 redis_password = ""
 
+r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)       
+
 @app.route("/redis_health")
 def hello_redis():
     """Example Hello Redis Program"""
     try:
         # The decode_repsonses flag here directs the client to convert the responses from Redis into Python strings
         # using the default encoding utf-8.  This is client specific.
-        r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-    
         r.set("msg:hello", "Hello Redis!!!")
         msg = r.get("msg:hello")
         return msg, 201        
@@ -24,35 +24,21 @@ def hello_redis():
         return e, 201
 
 # TODO: Redefine the tasks model, and create reusable collections of tasks
-# TODO: Create a database of tasks / Implement Redis
-# tasks = [
-#     {
-#         'id': 1,
-#         'title': u'Buy groceries',
-#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-#         'done': False
-#     },
-#     {
-#         'id': 2,
-#         'title': u'Learn Python',
-#         'description': u'Need to find a good Python tutorial on the web', 
-#         'done': False
-#     }
-# ]
-@app.route("/reddis/todo/api/v1.0/tasks", methods=["POST"])
+# TODO: Add date time
+
+@app.route("/todo/api/v1.0/tasks", methods=["POST"])
+# @auth.login_required
 def set_tasks_test():
     print(request.json)
-    if not request.json or not 'title' in request.json:
+    if not request.json or not 'title' in request.json or request.json['done']:
         abort(400)
     else:
         try:
             # The decode_repsonses flag here directs the client to convert the responses from Redis into Python strings
             # using the default encoding utf-8.  This is client specific.
-            r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)       
-            
             cat = request.json['category']
             hash_name = f"todos:{cat}:tasks"
-            r.hsetnx(hash_name, request.json["title"], str(request.json))
+            r.hset(hash_name, request.json["title"], str(request.json))
 
             msg = r.hgetall(hash_name)
             return jsonify(msg), 201
@@ -60,63 +46,50 @@ def set_tasks_test():
         except Exception as e:
             return e, 201
 
-@app.route("/get_test")
-def get_tasks_test():
-    r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-    msg = r.hgetall('todos:Practice:tasks')
-    return jsonify(msg), 201
-
-# Creates a URI field instead of id so the client is seeing URIs instead of IDs
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
-        else:
-            new_task[field] = task[field]
-    return new_task
-
 # TODO: Pagination argument
 # TODO: Filter options. Think about rows with which to filter
 # TODO: SEARCH
+
 # GET request for all task items in Database
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route("/todo/api/v1.0/tasks/all")
+# @auth.login_required
+def get_all_tasks():
+    msg = {}
+    categories = [cat for cat in r.scan_iter()]
+    for hash_name in categories:
+        print(hash_name)
+        try:
+            task = r.hgetall(hash_name)
+            print(task)
+            msg.update(task)
+        except:
+            pass
+    return jsonify(msg), 201
+
+# GET request for all tasks in specific categories
+@app.route("/todo/api/v1.0/tasks")
 # @auth.login_required
 def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+    print(request.json)
+    msg = {}
+    categories = request.json['category']
+    for cat in categories:
+        hash_name = f"todos:{cat}:tasks"
+        task = r.hgetall(hash_name)
+        msg.update(task)
+    return jsonify(msg), 201
 
 # GET request for single task item in Database
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+@app.route('/todo/api/v1.0/tasks/<category>/<title>', methods=['GET'])
 # @auth.login_required
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+def get_task(category, title):
+    print(request.json)
+    cat = request.json['category']
+    title = request.json['title']
+    hash_name = f"todos:{cat}:tasks"
+    msg = r.hget(hash_name, title)
+    return jsonify(msg), 201
 
-# POST request to add task item to database
-# TODO: Don't allow duplicate tasks by Title
-# TODO: Have a better system for incrementing IDs
-# TODO: urls by title slug
-
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-# @auth.login_required
-def create_task():
-    # request will pass the data, but only if it came marked as JSON
-    if not request.json or not 'title' in request.json:
-        # returns error for bad request
-        abort(400)
-    task = {
-        # manually auto incrementing task['id']
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        # defaulting description to none
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    # 201 code means successfully created
-    return jsonify({'task': task}), 201
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
