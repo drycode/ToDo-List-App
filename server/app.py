acf90b5b14ffb383cd04278cde294275acab161f
Dan Young <DanYoung@Christinas-iMac.fios-router.home>
2019-03-21 15:40:11 -0400

    Delete deprecated test functions

--- server/app.py
+++ server/app.py
@@ -85,7 +85,6 @@ def callback():
     response = auth.callback()
     json_obj = response.json
     json_obj['verified_email'] = str(json_obj['verified_email'])
-    # _redis_session_store(auth.session)
     return response
 
 @app.route("/logout")
@@ -112,26 +111,6 @@ def set_tasks():
             print("An exception was found")
             return e, 201
 
-# DEPRECATED
-@app.route("/todo/api/v1.0/tasks", methods=["POST"])
-@token_required
-def set_tasks_test():
-    print(request.json)
-    if not request.json or not 'title' in request.json or request.json['done']:
-        abort(400)
-    else:
-        try:
-            # The decode_repsonses flag here directs the client to convert the responses from Redis into Python strings
-            # using the default encoding utf-8.  This is client specific.
-            cat = request.json['category']
-            hash_name = f"todos:{cat}:tasks"
-            r.hset(hash_name, request.json["title"], str(request.json))
-
-            msg = r.hgetall(hash_name)
-            return jsonify(msg), 201
-
-        except Exception as e:
-            return e, 201
 
 ###################################################################################
 @app.route("/redis/tasks", methods=['GET'])
@@ -146,21 +125,6 @@ def get_all_tasks():
     except Exception as e:
         return e, 201
 
-# DEPRECATED
-@app.route("/todo/api/v1.0/tasks", methods=['GET'])
-@token_required
-def get_all_tasks_test():
-    msg = {}
-    categories = [cat for cat in r.scan_iter()]
-    for hash_name in categories:
-        print(hash_name)
-        try:
-            task = r.hgetall(hash_name)
-            print(task)
-            msg.update(task)
-        except:
-            pass
-    return jsonify(msg), 201
 
 ###################################################################################
 # GET request for all tasks in specific categories
@@ -179,18 +143,6 @@ def get_tasks_cat(category):
     #     print("an exception has occurred")
     #     return e, 201
 
-# DEPRECATED
-@app.route("/todo/api/v1.0/tasks", methods=['GET'])
-@token_required
-def get_tasks_cat_test():
-    print(request.json)
-    msg = {}
-    categories = request.json['category']
-    for cat in categories:
-        hash_name = f"todos:{cat}:tasks"
-        task = r.hgetall(hash_name)
-        msg.update(task)
-    return jsonify(msg), 201
 
 ###################################################################################
 
@@ -215,19 +167,6 @@ def delete_task(category, title):
         # msg = {"msg":"Invalid deletion parameters. Please try again."}
         # return jsonify(msg)
     
-# DEPRECATED
-@app.route('/todo/api/v1.0/tasks/<category>/<title>/delete', methods=['DELETE'])
-@token_required
-def delete_task_test(category, title):
-    hash_name = f"todos:{category}:tasks"
-    if not r.hexists(hash_name, title):
-        abort(404)
-    try: 
-        r.hdel(hash_name, title)
-        msg = "Delete was successful"
-    except:
-        msg = "Something went wrong"
-    return msg, 201
 
 ###################################################################################
 # TODO: implement multi-delete
@@ -238,14 +177,6 @@ def delete_multiple_tasks():
     return get_all_tasks()
 
 ###################################################################################
-# Possibly Deprecated.....
-def _redis_session_store(session):
-    auth.session['session_id'] = _get_active_user().get_user_id()
-    hash_key = "session_data:{}:session_info".format(auth.session['session_id'])
-    r.hset(hash_key, "access_token", auth.session['oauth_state']['access_token'])
-    r.hset(hash_key, "id_token", auth.session['oauth_state']['id_token'])
-    r.expire(hash_key, int(auth.session['oauth_state']['expires_in']))
-
 
 @app.errorhandler(404)
 def not_found(error):
