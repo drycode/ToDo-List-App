9b25fbea8374d3b44e0cf7f3d3ca9c3c1cbe6398
Dan Young <DanYoung@Christinas-iMac.fios-router.home>
2019-04-05 08:24:27 -0400

    Rename src to api

---
+++ api/server/redis/redis_methods.py
@@ -0,0 +1,198 @@
+"""This module contains CRUD methods for the flask API instantiated
+at app.py. These methods are designed to work with a running instance
+of redis."""
+
+from datetime import datetime
+from hashlib import blake2b
+
+from server.redis.redis_local import redis_instance
+
+# TODO: Include subtasks in Hash model
+class ToDoUser:
+    """
+    All ToDo list tasks are centralized in this ToDoUser class.
+    """
+
+    def __init__(self, user_obj, r=redis_instance):
+        self.r = r
+        self.userid = user_obj["id"]
+        self.name = user_obj["name"]
+        self.mykey = "Todos:users:" + str(self.userid) + ":tasks:"
+        self.my_task_ids = self.mykey + "all_task_ids"
+        self.r.hmset("Todos:users:" + str(self.userid), user_obj)
+
+    # Create Methods
+    def set_task(self, task_obj):
+        """
+        Establishes key value stores in redis for rapid querying of task_ids for specified
+        queries.
+        Task_ids are used in subsequent queries for accessing hash map of specific task
+        field data
+        """
+        task_id, task_obj, task_hash_key = self._initialize_redis_task(task_obj)
+        self._initialize_redis_hashmap(task_hash_key, task_obj)
+        self._set_task(task_id, task_obj)
+
+    # Retrieve Methods
+
+    def get_all_tasks(self):
+        """
+        Returns a generator object with hash results of all available task_ids.
+        """
+        tasks = self.r.smembers(self.my_task_ids)
+        for item in self._get_tasks(tasks):
+            yield item
+
+    def get_one_task(self, title):
+        """
+        Returns a single task object from a given task title.
+        Titles must be unique, regardless of categories.
+        """
+        task_id = str(_blake2b_hash_title(title))
+        task = self.r.hgetall(self.mykey + task_id)
+        return task
+
+    def get_category_tasks(self, category):
+        """
+        Returns a generator object with hash results of task_id keys with given ``category``
+
+        ``category`` should be passed as a string.
+        """
+        tasks = self.r.smembers(self.mykey + category + "_task_ids")
+        for item in self._get_tasks(tasks):
+            yield item
+
+    def get_duedate_range(self, start, end):
+        """
+        Returns a generator object with the hash results of task_id keys with due dates
+        between ``start`` and ``end``.
+
+        ``start`` and ``end`` should be passed as datetime objects in python.
+        """
+        # start, end = map(self._convert_datetime, (start, end))  ### DEPRECATED
+        tasks = self.r.zrangebyscore(self.mykey + "due_sort_all_task_ids", start, end)
+        for item in self._get_tasks(tasks):
+            yield item
+
+    def get_user_id(self):
+        """Returns active user_id"""
+        return self.userid
+
+    # Delete Methods
+    def delete_tasks(self, titles):
+        """
+        Must give the category along with a potential list of task_ids
+        """
+        tasks_data = []
+        for title in titles:
+            tasks_data.append(self._delete_one_task(title))
+
+        for task_id, category in tasks_data:
+            hash_key = self.mykey + str(task_id)
+            self.r.hdel(hash_key, "*")
+            self.r.srem(self.my_task_ids, task_id)
+            self.r.srem(self.mykey + str(category) + "_task_ids", task_id)
+            self.r.zrem(self.mykey + "due_sort_all_task_ids", task_id)
+
+    # Class Based Helper Function
+
+    def _set_task(self, task_id, task_obj):
+        """Stores all task_ids at self.mykey:all_task_ids.
+
+        Stores by category at self.mykey:<category>_task_ids.
+
+        Stores by due_date in ascending order at
+        self.mykey:due_sort_all_taks_ids:<task_id>:<int_due_date> if due date is available"""
+        self.r.sadd(self.my_task_ids, task_id)
+        self.r.sadd(self.mykey + task_obj["category"] + "_task_ids", task_id)
+        print(task_obj["due_date"])
+        if task_obj["due_date"]:
+            self.r.zadd(
+                self.mykey + "due_sort_all_task_ids", {task_id: task_obj["due_date"]}
+            )
+
+    def _initialize_redis_hashmap(self, task_hash_key, task_obj):
+        """Creates a hash_map of field value pairs for a particular user's task stored
+        At location self.mykey:<task_id>."""
+        self.r.hmset(task_hash_key, task_obj)
+
+    def _initialize_redis_task(self, task_obj):
+        """Hashes task title, returns task_id. Also processes dates and compiles
+        task_hash_key for redis database"""
+        task_id = str(_blake2b_hash_title(task_obj["title"]))
+
+        task_obj["date_created"], task_obj["due_date"] = _convert_dates(task_obj)
+
+        task_hash_key = self.mykey + task_id
+        return task_id, task_obj, task_hash_key
+
+    def _get_tasks(self, task_ids):
+        return (self.r.hgetall(self.mykey + str(task)) for task in task_ids)
+
+    def _set_sub_tasks(self, task_id, *subtasks):
+        print(*subtasks)
+        current = self._get_sub_tasks(task_id)
+        print(current)
+        self.r.rpush(self.mykey + task_id + ":subtasks", *subtasks)
+
+    def _get_sub_tasks(self, task_id):
+        subtask_list = self.r.lrange(
+            self.mykey + task_id + ":subtasks",
+            0,
+            self.r.llen(self.mykey + task_id + ":subtasks"),
+        )
+        return subtask_list
+
+    def _delete_one_task(self, title):
+        """
+        Deletes a single task given a specific task title.
+        """
+        task_id = str(_blake2b_hash_title(title))
+        category = self.r.hmget(self.mykey + task_id, "category")
+        return task_id, category[0]
+
+    # Dunder Methods
+
+    def __repr__(self):
+        tot_tasks = self.r.scard(self.my_task_ids)
+        if tot_tasks:
+            return "User(user_id: %r, name: %r, tasks: %r)" % (
+                self.userid,
+                self.name,
+                tot_tasks,
+            )
+        return "User(user_id: %r, name: %r, tasks: 0)" % (self.userid, self.name)
+
+
+# General Helper Functions
+def _convert_dates(task_obj):
+    """Creates integer value from datetime object, and processes missing values"""
+    due_str = task_obj.get("due_date", "")
+    print(due_str)
+
+    format_template = "%Y%m%d%H%M%S"
+    created_str = task_obj.get(
+        "date_created", datetime.strftime(datetime.utcnow(), format_template)
+    )
+    print(due_str)
+    if due_str:
+        return int(created_str), int(due_str)
+    return int(created_str), ""
+
+
+def _stringify_datetime(date):
+    year, month, day, hour, minute, second = map(
+        int, (date[0:4], date[4:6], date[6:8], date[8:10], date[10:12], date[12:14])
+    )
+    date = datetime(year, month, day, hour, minute, second).strftime(
+        "Date: %m-%d-%Y Time: %H:%M:%S"
+    )
+    return date
+
+
+def _blake2b_hash_title(string):
+    hashed_title = blake2b(digest_size=10)
+    title = bytes(string, encoding="utf-8")
+    hashed_title.update(title)
+    task_id = hashed_title.hexdigest()
+    return task_id
---
+++ api/test_env/test_flask_server.py
@@ -0,0 +1,183 @@
+from datetime import datetime
+from types import GeneratorType
+
+from pytest import raises, mark
+from fakeredis import FakeStrictRedis
+
+
+from server.redis.redis_methods import (
+    ToDoUser,
+    _blake2b_hash_title,
+    _convert_dates,
+    _stringify_datetime,
+)
+
+
+red_instance = FakeStrictRedis()
+
+user_obj = {
+    "email": "testytest@gmail.com",
+    "family_name": "Fredericks",
+    "given_name": "Brian",
+    "id": "230872340987520234985",
+    "link": "https://plus.google.com/230872340987520234985",
+    "locale": "en",
+    "name": "Brian Fredericks",
+    "picture": "https://lh3.googleusercontent.com/photo.jpg",
+    "verified_email": "true",
+}
+
+task_objs = [
+    {
+        "title": "Do your laundry",
+        "category": "chores",
+        "date_created": "201901201518",
+        "due_date": "201901201519",
+    },
+    {
+        "title": "Play Saxophone",
+        "category": "Practice",
+        "date_created": "201804201934",
+        "due_date": "201901201520",
+    },
+    {
+        "title": "Grocery Shop",
+        "category": "chores",
+        "date_created": "201901201521",
+        "due_date": "",
+    },
+    {
+        "title": "Play Clarinet",
+        "category": "Practice",
+        "date_created": "201901201523",
+        "due_date": "201901201524",
+    },
+]
+
+
+user = ToDoUser(user_obj, r=red_instance)
+
+for i, task in enumerate(task_objs):
+    user.set_task(task_objs[i])
+
+
+class TestToDoUser:
+    def test_get_all_tasks(self):
+        x = user.get_all_tasks()
+        assert isinstance(x, GeneratorType) == True
+        assert isinstance(next(x), object) == True
+
+    def test_get_one_task(self):
+        actual = user.get_one_task("Grocery Shop")
+        print(actual)
+        assert actual == {
+            b"title": b"Grocery Shop",
+            b"category": b"chores",
+            b"date_created": b"201901201521",
+            b"due_date": b"",
+        }
+
+    def test_get_category_tasks(self):
+        x = user.get_category_tasks("Practice")
+        assert isinstance(x, GeneratorType) == True
+        assert isinstance(next(x), object) == True
+
+    def test_get_duedate_range(self):
+        x = user.get_duedate_range(201901201518, 201901201524)
+        assert isinstance(x, GeneratorType) == True
+        assert isinstance(next(x), object) == True
+        # Check the items in the generator
+        assert sum(1 for i in x) == 2
+
+    def test_get_user_id(self):
+        assert user.get_user_id() == "230872340987520234985"
+
+    def test__set_subtasks(self):
+        assert (
+            user._set_sub_tasks("fb4b0dc2756d5131a6f5", "Tomatos", "Brocolli", "Meat")
+            == None
+        )
+
+    def test__get_subtasks(self):
+        assert user._get_sub_tasks("fb4b0dc2756d5131a6f5") == [
+            b"Tomatos",
+            b"Brocolli",
+            b"Meat",
+        ]
+
+    @mark.parametrize(
+        "user_input,expected",
+        {
+            (
+                ToDoUser(user_obj, red_instance),
+                "User(user_id: '230872340987520234985', name: 'Brian Fredericks', tasks: 4)",
+            ),
+            (
+                ToDoUser(
+                    {
+                        "email": "notasks@gmail.com",
+                        "family_name": "Jones",
+                        "given_name": "Jeffrey",
+                        "id": "230872345620234985",
+                        "link": "https://plus.google.com/230872345620234985",
+                        "locale": "en",
+                        "name": "Jeffrey Jones",
+                        "picture": "https://lh3.googleusercontent.com/photo.jpg",
+                        "verified_email": "true",
+                    },
+                    red_instance,
+                ),
+                "User(user_id: '230872345620234985', name: 'Jeffrey Jones', tasks: 0)",
+            ),
+        },
+    )
+    def test__repr__(self, user_input, expected):
+        assert user_input.__repr__() == (expected)
+
+    def test__delete_one_task(self):
+        actual = user._delete_one_task("Grocery Shop")
+        assert actual == ("fb4b0dc2756d5131a6f5", b"chores")
+
+    def test_delete_tasks(self):
+        user.delete_tasks(["Grocery Shop"])
+        assert sum(1 for i in user.get_all_tasks()) == 3
+
+    # @mark.parametrize("task_obj_input,expected", {(task_objs[0], 0)})
+    def test__initialize_redis_task(self):
+        assert user._initialize_redis_task(task_objs[0]) == (
+            "0480a119a478b410bba4",
+            {
+                "title": "Do your laundry",
+                "category": "chores",
+                "date_created": 201901201518,
+                "due_date": 201901201519,
+            },
+            "Todos:users:230872340987520234985:tasks:0480a119a478b410bba4",
+        )
+
+    def test__get_tasks(self):
+        x = user._get_tasks([0, 1, 2, 3])
+        assert isinstance(x, GeneratorType) == True
+        assert next(x) == {}
+
+    @mark.parametrize(
+        "hash_input,expected",
+        {
+            ("check this out", "f7d32a9739d22c399cc0"),
+            ("1-0918234-34-098235098723-#$)(*@&#$%)(*&!#%_(*", "2c9af44e7aab5c9c7a20"),
+        },
+    )
+    def test__hash(self, hash_input, expected):
+        assert _blake2b_hash_title(hash_input) == expected
+
+    def test__convert_dates(self):
+        assert _convert_dates(task_objs[3]) == (201901201523, 201901201524)
+        assert _convert_dates(task_objs[2]) == (201901201521, "")
+
+    @mark.parametrize(
+        "intdate_input,expected",
+        {("20190330185242", "Date: 03-30-2019 Time: 18:52:42")},
+    )
+    def test__stringify_datetime(self, intdate_input, expected):
+        assert _stringify_datetime(intdate_input) == expected
+
