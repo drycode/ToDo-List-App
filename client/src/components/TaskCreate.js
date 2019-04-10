0a5cc3394377a7834127e03ce87ab1a1dd411125
Dan Young <DanYoung@Christinas-iMac.fios-router.home>
2019-04-10 16:47:59 -0400

    Further implementation of Task Creation form

--- client/src/components/TaskCreate.js
+++ client/src/components/TaskCreate.js
@@ -2,42 +2,71 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class TaskCreate extends React.Component {
-  renderInput({ input, placeholder }) {
-    return <input type="text" placeholder={placeholder} {...input} />;
+  renderInput({ input, placeholder, meta }) {
+    console.log(meta);
+    return (
+      <>
+        <div className="ui fluid input">
+          <input type="text" placeholder={placeholder} {...input} />
+          <div
+            className="ui icon button"
+            data-tooltip="Add an optional due date to your task"
+          >
+            <i class="calendar icon button" />
+          </div>
+        </div>
+        {/* <div>
+          <div class="ui error message">
+            <div class="header">{meta.error}</div>
+          </div>
+        </div> */}
+      </>
+    );
+  }
+  renderCategoryInput({ input, placeholder, meta }) {
+    console.log(meta);
+    return (
+      <>
+        <div className="ui fluid input">
+          <input type="text" placeholder={placeholder} {...input} />
+        </div>
+        {/* <div>
+          <div class="ui error message">
+            <div class="header">{meta.error}</div>
+          </div>
+        </div> */}
+        <button className="ui button primary" style={{ float: "right" }}>
+          Submit
+        </button>
+      </>
+    );
   }
 
   renderForm() {
     return (
       <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <h4 style={{ textAlign: "left" }}>Create New Task</h4>
-        <div className="ui fluid input">
+        <div>
           <Field
             name="title"
             component={this.renderInput}
             placeholder="Add a task..."
           />
-          <div
-            class="ui icon button"
-            data-tooltip="Add an optional due date to your task"
-          >
-            <i class="calendar icon button" />
-          </div>
         </div>
-        <div className="ui fluid input">
+        <div>
           <Field
             name="category"
-            component={this.renderInput}
+            component={this.renderCategoryInput}
             placeholder="...and a category"
           />
           {/* <Field name="due_date" component={this.renderInput} /> */}
-          <button className="ui button primary">Submit</button>
         </div>
       </form>
     );
   }
 
   onSubmit(formValues) {
-    console.log(formValues);
+    // console.log(formValues);
   }
 
   render() {
@@ -45,6 +74,19 @@ class TaskCreate extends React.Component {
   }
 }
 
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+  if (!formValues.category) {
+    errors.category = "You must enter a category";
+  }
+
+  return errors;
+};
+
 export default reduxForm({
-  form: "taskCreate"
+  form: "taskCreate",
+  validate
 })(TaskCreate);
